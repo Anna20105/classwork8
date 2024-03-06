e182e4eb7b14d0e12a17d01bae223e3b7a9f5c0c
Anna20105 <157532133+Anna20105@users.noreply.github.com>
2024-03-06 09:27:24 +0000

    add to min.py

---
+++ min.py
@@ -0,0 +1,11 @@
+def min_n_variables(elements):
+    min_v = elements(0)
+    for x in elements:
+        if x = min_v:
+             min_v=x
+    return min_v         
+
+   # elements=[6**6,7**5,5**7]
+    elements=[5,1,9,8,7]
+assert min_n_variables(elemets) == 0
+print('min value of elements =',min_n_variables(elements))
